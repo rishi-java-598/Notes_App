4681d68e9b94f81640295ee8fe6b76f0bd01fc79
rishi-java-598 <rishiraj.sharma.javaventures@gmail.com>
2025-09-22 13:53:52 -0400

    final

--- server/server.js
+++ server/server.js
@@ -5,6 +5,9 @@ import authRoutes from "./routes/authRoutes.js";
 import noteRoutes from "./routes/noteRoutes.js";
 import { notFound, errorHandler } from "./middlewares/errorMiddleware.js";
 import cors from 'cors'
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 
 dotenv.config();
 
@@ -14,6 +17,15 @@ app.use(express.json());
 app.use(cors());
 app.use("/api/auth", authRoutes);
 app.use("/api/notes", noteRoutes);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+app.use(express.static(path.join(__dirname, "../client/dist"))); // CRA uses 'build'
+
+// For any other route, send index.html
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/dist/index.html"));
+});
+
 
 app.use(notFound);
 app.use(errorHandler);
