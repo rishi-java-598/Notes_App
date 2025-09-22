3cc7d21b42282ce348d894a48fc2342973355a4e
rishi-java-598 <rishiraj.sharma.javaventures@gmail.com>
2025-09-22 14:56:18 -0400

    Prepare for Render deployment

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:5000/api";
+const BASE_URL = "https://notes-app-6-bc3x.onrender.com/api";
 
 export const request = async (path, options = {}) => {
   const token = localStorage.getItem("token");
